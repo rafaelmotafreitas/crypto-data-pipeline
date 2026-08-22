d84e10dda0327ba458db188776a7afe13366d8a7
rafaelmotafreitas <rafaelmotta200@gmail.com>
2026-08-22 10:02:03 -0300

    extracao e transformacao dos dados

--- extract.py
+++ extract.py
@@ -1,29 +1,55 @@
 # importação da biblioteca requests
 
 import requests
+from datetime import datetime, timezone
 
-# url do site
+def extrair_dados_crypto():
 
-url = "https://api.coingecko.com/api/v3/simple/price"
+    # url do site 
+    url = "https://api.coingecko.com/api/v3/simple/price"
 
-# dict de parametros
+    # parametros
+    params = {
+        'ids':'bitcoin,solana',
+        'vs_currencies':'usd'
+    }
 
-params = {
-    'ids': 'bitcoin,solana',
-    'vs_currencies': 'usd'
-} 
+    # fazendo a requisicao
+    request = requests.get(url, params=params)
 
-# fazer a requisição agora
+    #convertendo para json
 
-request = requests.get(url, params=params)
+    dados = request.json()
 
-# transformar a resposta em json
+    # atribuindo o tempo
+    tempoDaExtracao = datetime.now(timezone.utc)
 
-dados = request.json()
+    #exibição na tela
+    print(f"Moedas solicitadas: {dados}")
 
-# exibição na tela
+    # dicionario de de dados 
 
-print(dados)
+    dadosTratados = [
+        {
+            "moeda": "bitcoin",
+            "valor": dados['bitcoin']['usd'],
+            "data_extracao": tempoDaExtracao.isoformat()
+        },
+        {
+            "moeda": "solana",
+            "valor": dados['solana']['usd'],
+            "data_extracao": tempoDaExtracao.isoformat()
+        }
+    ]
 
-print(dados['bitcoin']['usd'])
-print(dados['solana']['usd'])
+    # retorno do dicionario
+
+    return dadosTratados
+
+
+resultado = extrair_dados_crypto()
+
+print(resultado)
+
+
+
