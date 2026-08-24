4a539b55f9e96745628cbdbfac6c813a80a88409
rafaelmotafreitas <rafaelmotta200@gmail.com>
2026-08-24 18:30:33 -0300

    melhora do codigo de extracao + conexao e insercao num postgre local

--- extract.py
+++ extract.py
@@ -1,55 +1,72 @@
-# importação da biblioteca requests
+# modulos e importacao
 
+import os
 import requests
 from datetime import datetime, timezone
+import psycopg2
+from dotenv import load_dotenv
 
-def extrair_dados_crypto():
+# setup de inicializacao das senhas
+load_dotenv()
+senhaBancoDeDados = os.getenv("DB_PASSWORD")
 
-    # url do site 
-    url = "https://api.coingecko.com/api/v3/simple/price"
+# extracao de dados da api
 
-    # parametros
-    params = {
-        'ids':'bitcoin,solana',
-        'vs_currencies':'usd'
-    }
+print('Buscando precos na CoinGecko....')
+url = 'https://api.coingecko.com/api/v3/simple/price'
+params = {
+    'ids':'bitcoin,ethereum',
+    'vs_currencies':'usd'
+}
+# pegando os dados e transformando em json
+resposta = requests.get(url, params=params)
+dadosBrutos = resposta.json()
 
-    # fazendo a requisicao
-    request = requests.get(url, params=params)
+dataExtracao = datetime.now(timezone.utc).isoformat()
 
-    #convertendo para json
+dadosTransformados = []
 
-    dados = request.json()
+# preenchimento com os dados
 
-    # atribuindo o tempo
-    tempoDaExtracao = datetime.now(timezone.utc)
-
-    #exibição na tela
-    print(f"Moedas solicitadas: {dados}")
-
-    # dicionario de de dados 
-
-    dadosTratados = [
-        {
-            "moeda": "bitcoin",
-            "valor": dados['bitcoin']['usd'],
-            "data_extracao": tempoDaExtracao.isoformat()
-        },
-        {
-            "moeda": "solana",
-            "valor": dados['solana']['usd'],
-            "data_extracao": tempoDaExtracao.isoformat()
-        }
-    ]
-
-    # retorno do dicionario
-
-    return dadosTratados
+for moeda, valores in dadosBrutos.items():
+    dadosTransformados.append({
+        "moeda": moeda,
+        "valor": valores["usd"],
+        "data_extracao": dataExtracao
+    })
 
 
-resultado = extrair_dados_crypto()
+# carregamento dos dados no postgre
 
-print(resultado)
+print("Conectando ao PostgreSQL....")
 
+try:
+    conexao = psycopg2.connect(
+        host="localhost",
+        database="crypto_pipeline",
+        user="postgres",
+        password=senhaBancoDeDados
+    )
+    cursor = conexao.cursor()
 
+    # O comando SQL com os %s (Placeholders) para segurança contra injeção de código
+    sql_insert = """
+        INSERT INTO precos_cripto (moeda, valor, data_extracao)
+        VALUES (%s, %s, %s);
+    """
 
+    print("Inserindo dados no banco...")
+    for item in dadosTransformados:
+        # Executa o SQL trocando os %s pelos valores reais da nossa lista
+        cursor.execute(sql_insert, (item["moeda"], item["valor"], item["data_extracao"]))
+
+    # Salva as alterações definitivamente
+    conexao.commit()
+    print("Sucesso! O Pipeline ETL rodou perfeitamente. 🎉")
+
+    # Fecha as portas
+    cursor.close()
+    conexao.close()
+
+except Exception as erro:
+    print(f"Ops! Deu algum erro no banco: {erro}")
